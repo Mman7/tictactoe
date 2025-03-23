ec6bba3673a261f741a806d4e120b307dcc8f5fc
ericYz <youzai0427@gmail.com>
2025-03-23 19:37:51 +0800

    intialize

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,47 @@
+﻿using Game;
+
+class Program
+{
+    static void Main()
+    {
+        TicTacToe game = new TicTacToe();
+        ConsoleKeyInfo input;
+
+        for (game.gameRound = 0; game.gameRound < game.endRound; game.gameRound++)
+        {
+            do
+            {
+                game.renderTemplete();
+                Console.WriteLine($"Round {game.gameRound + 1} Player {game.turn} turn!");
+                Console.WriteLine("Select the number between 0-8 at empty grid");
+                input = Console.ReadKey();
+                Console.WriteLine("\n");
+            } while (
+                checkInput(input)
+                || checkIsEmpty(game.gridList[int.Parse(input.KeyChar.ToString())].Shape)
+            );
+            int selected = int.Parse(input.KeyChar.ToString());
+            game.changeGridShape(selected, game.turn);
+            game.renderTemplete();
+            if (game.checkWin())
+            {
+                Console.WriteLine("check win mF");
+                break;
+            }
+            game.switchTurn();
+        }
+    }
+
+    /// <summary>
+    ///  [input] is not number or bigger than 8 return true
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    static bool checkInput(ConsoleKeyInfo input)
+    {
+        return !int.TryParse(input.KeyChar.ToString(), out int result)
+            || int.Parse(input.KeyChar.ToString()) > 8;
+    }
+
+    static bool checkIsEmpty(GridShape listItem) => listItem != GridShape.empty;
+}
---
+++ TicTacToe/tic-tac-toe.cs
@@ -0,0 +1,108 @@
+//TODO implement win condition
+
+namespace Game
+{
+    public class TicTacToe
+    {
+        public int gameRound;
+        public int endRound = 9;
+        public GridShape turn = GridShape.O;
+
+        public Grid[] gridList =
+        {
+            new Grid(GridShape.empty),
+            new Grid(GridShape.empty),
+            new Grid(GridShape.empty),
+            new Grid(GridShape.empty),
+            new Grid(GridShape.empty),
+            new Grid(GridShape.empty),
+            new Grid(GridShape.empty),
+            new Grid(GridShape.empty),
+            new Grid(GridShape.empty),
+            new Grid(GridShape.empty),
+        };
+
+        public void switchTurn()
+        {
+            turn = turn == GridShape.O ? GridShape.X : GridShape.O;
+        }
+
+        public void changeGridShape(int gridListIndex, GridShape shape)
+        {
+            gridList[gridListIndex].Shape = shape;
+        }
+
+        ///---------------------------------------------
+        public void renderTemplete()
+        {
+            Console.Clear();
+            Console.WriteLine(
+                @$"{renderShape(gridList[0], 0)} | {renderShape(gridList[1], 1)} | {renderShape(gridList[2], 2)} 
+----------
+{renderShape(gridList[3], 3)} | {renderShape(gridList[4], 4)} | {renderShape(gridList[5], 5)}
+----------
+{renderShape(gridList[6], 6)} | {renderShape(gridList[7], 7)} | {renderShape(gridList[8], 8)}
+"
+            );
+        }
+
+        ///---------------------------------------------
+
+        string renderShape(Grid grid, int idx)
+        {
+            if (grid.Shape == GridShape.empty)
+                return $"{idx}";
+            return grid.Shape.ToString();
+        }
+
+        public bool checkWin()
+        {
+            if (gameRound < 3)
+                return false;
+            //TODO finish this
+            if (allSame(gridList[0], gridList[1], gridList[2]))
+                return true;
+            if (allSame(gridList[0], gridList[1], gridList[2]))
+                return true;
+            if (allSame(gridList[0], gridList[1], gridList[2]))
+                return true;
+            if (allSame(gridList[0], gridList[1], gridList[2]))
+                return true;
+            if (allSame(gridList[0], gridList[1], gridList[2]))
+                return true;
+            if (allSame(gridList[0], gridList[1], gridList[2]))
+                return true;
+            if (allSame(gridList[0], gridList[1], gridList[2]))
+                return true;
+            if (allSame(gridList[0], gridList[1], gridList[2]))
+                return true;
+
+            return false;
+        }
+
+        bool allSame(Grid item1, Grid item2, Grid item3)
+        {
+            if (new[] { item1.Shape, item2.Shape, item3.Shape }.All(x => x == GridShape.empty))
+                return false;
+
+            return item1.Shape == item2.Shape && item2.Shape == item3.Shape;
+        }
+    }
+}
+
+public class Grid
+{
+    public GridShape Shape { get; set; }
+
+    public Grid(GridShape shape)
+    {
+        Shape = shape;
+    }
+}
+
+public enum GridShape
+{
+    X,
+    O,
+    empty,
+}
